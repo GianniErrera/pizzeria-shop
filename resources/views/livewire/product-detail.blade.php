1887e23aceb535faab1cd4d17d0f02eb0c298975
Gianni Errera <giannierrera@yahoo.it>
2021-10-13 18:19:00 +0200

    A few layout adjustments

--- resources/views/livewire/product-detail.blade.php
+++ resources/views/livewire/product-detail.blade.php
@@ -13,19 +13,23 @@
         <div class="d-flex justify-content-between my-2">
             <div class="text-capitalize"><h2>{{ $product->name }} - €{{ $product->price }}</h2></div>
             <div>
-            <div>
-                <button wire:click.prevent="add">+</button>
-                <button wire:click.prevent="subtract">-</button>
+
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal-lg-{{$product->id}}">Close</button>
+        </div>
+        <div class="d-flex justify-content-between my-2">
+            <div class="align-middle">
+                <button wire:click.prevent="add"><img src="images/plus_button.png" alt="" width="40px"></button>
+                <button wire:click.prevent="subtract"><img src="images/minus_button.png" alt="" width="40px"></button>
             </div>
-            <input class="form-control" wire:model="quantity" name="quantity" type="number" min="1" max="100" class="number mx-4" value={{$quantity}} type="text">
+            <input class="form-control form-control-lg"
+                wire:model="quantity"
+                name="quantity"
+                min="1" max="100"
+                class="number mx-4"
+                value={{$quantity}}
+                type="text"
+                style="width: 3em">
             </div>
-            <div><button
-                type="button"
-                class="btn-close m-4"
-                data-mdb-dismiss="modal"
-                aria-label="Close"
-              >x</button></div>
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal-lg-{{$product->id}}">Close</button>
         </div>
         <hr>
         <div class="mb-4 ml-4">
